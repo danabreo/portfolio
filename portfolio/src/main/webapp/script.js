bdaa566d6bd4af67bcb8fee18d47cdaa653c24c4
Daniel Abreo <danielabreo@google.com>
2020-06-02 17:05:49 +0000

    Populate greeting from server

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,3 +1,10 @@
 function changeImage() {
   document.getElementById("switch").className = "index-image-2"; 
 }
+
+async function populateGreeting() {
+  const response = await fetch('/data');
+  const greeting = await response.text();
+  document.getElementById("greeting").innerText = greeting;
+}
+populateGreeting();
