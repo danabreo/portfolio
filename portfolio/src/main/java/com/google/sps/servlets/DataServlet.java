b27b811612bfcacd20239d9021f4a3515e66aadc
Daniel Abreo <danielabreo@google.com>
2020-06-03 14:17:41 +0000

    Replace server-fetched greeting; Add STEP section populated with JSON

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private ArrayList<String> data;
+
+  @Override
+  public void init() {
+    data = new ArrayList<String>();
+    data.add("Malcolm from NC");
+    data.add("Sabrina from FL");
+    data.add("Dan from TX");
+  }
+
+  private String convertToJson(ArrayList<String> messages) {
+    String json = "{ \"messages\" : [\"";
+    json += String.join("\",\"",messages);
+    json += "\"] }";
+    return json;
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Howdy! I'm Dan,");
+    // Convert messages to JSON
+    String json = convertToJson(data);
+
+    // Send JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -2,9 +2,18 @@ function changeImage() {
   document.getElementById("switch").className = "index-image-2"; 
 }
 
-async function populateGreeting() {
-  const response = await fetch('/data');
-  const greeting = await response.text();
-  document.getElementById("greeting").innerText = greeting;
+function createListElement(text) {
+  const listElement = document.createElement('li');
+  listElement.innerText = text;
+  return listElement;
 }
-populateGreeting();
+
+function populateMembers() {
+  fetch('/data').then(response => response.json()).then((data) => {
+    const podMatesElement = document.getElementById('pod-mates');
+    data.messages.forEach(message => {
+      podMatesElement.appendChild(createListElement(message));
+    });
+  });
+}
+populateMembers();
